bcf9ec657960ce14961b82a17c9921324a91560d
Rana-usman-ul-haq <98952692+Rana-usman-ul-haq@users.noreply.github.com>
2023-05-06 16:47:34 +0500

    Update token.sol

--- token.sol
+++ token.sol
@@ -729,8 +729,8 @@ contract Coin is Context, IERC20, Ownable {
     
     constructor() {
         _rOwned[owner()] = _rTotal;
-        //IUniswapV2Router02 _uniswapV2Router = IUniswapV2Router02(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D); // mainnet router address
-        IUniswapV2Router02 _uniswapV2Router = IUniswapV2Router02(0xD99D1c33F9fC3444f8101754aBC46c52416550D1); // testnet router address
+        IUniswapV2Router02 _uniswapV2Router = IUniswapV2Router02(0x10ED43C718714eb63d5aA57B78B54704E256024E); // mainnet router address
+        //IUniswapV2Router02 _uniswapV2Router = IUniswapV2Router02(0xD99D1c33F9fC3444f8101754aBC46c52416550D1); // testnet router address
         
         // Create a uniswap pair for this new token
         uniswapV2Pair = IUniswapV2Factory(_uniswapV2Router.factory())
